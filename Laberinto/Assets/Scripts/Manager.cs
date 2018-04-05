909f3ac5c145568dfb9a896b5c4830484e6d93ce
Carlos Salvador Garza Garza <CarlosGoogles@users.noreply.github.com>
2018-04-05 04:29:42 -0500

    proyecto completo
    .

---
+++ Laberinto/Assets/Scripts/Manager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+
+	private int tiempoTranscurrido = 0;
+    private int tiempoInicio = 0;
+
+    private bool pause = false;
+
+    public Text tiempo;
+
+
+    // Use this for initialization
+    void Start () {
+        tiempoInicio = (int)Time.time;
+        pause = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UpdatePause();
+        UpdateTime();
+    }
+
+    void UpdatePause()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            pause = !pause;
+
+            System.Threading.Thread.Sleep(250);
+        }
+
+        if (pause)
+        {
+            Time.timeScale = 0;
+            if (Input.GetKey(KeyCode.Q))
+            {
+                Application.Quit();
+            }
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    void UpdateTime()
+    {
+        tiempoTranscurrido = (int) (Time.time - tiempoInicio);
+        int segundos = tiempoTranscurrido % 60;
+        int minutos = tiempoTranscurrido / 60;
+        string seg = segundos.ToString();
+        string min = minutos.ToString();
+
+        if (segundos< 10)
+        {
+            seg = "0" + seg;
+        }
+        if (minutos< 10)
+        {
+            min = "0" + min;
+        }
+
+        tiempo.text = "Time: " + min + ":" + seg;
+    }
+}
---
+++ Laberinto/Assets/Scripts/Plane.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour {
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.left, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+        }
+    }
+}
